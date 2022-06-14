1159a3f2e25863389750f8a4f4bab5d2a4acee20
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2022-06-14 19:26:53 +0300

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Swift Learning on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
